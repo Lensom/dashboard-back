69caa4c76dab9e9ca624bdc87c8919c2ba0a6d31
Magura <magurawork@gmail.com>
2023-08-07 23:44:06 +0300

    Refactoring: routes - users/create to user/registration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { User } from '../models/user.model';
 import { AuthService } from './auth.service';
 
-@Controller('users')
+@Controller('user')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('create')
+  @Post('registration')
   registration(@Body() userData: User) {
     return this.authService.registration(userData);
   }
